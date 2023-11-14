277c4f3b0ccf4af731064b1cbc870446297b534f
Gioele <gioelebenci@live.com>
2023-11-15 00:02:52 +0100

    updates code structure

--- scripts/movement/MissileMovement.gd
+++ scripts/movement/MissileMovement.gd
@@ -6,56 +6,47 @@ extends RigidBody2D
 signal input_started
 signal input_ended
 
-var _default_direction : Vector2 = Vector2.UP
-var _input_direction : Vector2 = _default_direction
 var _is_inputting : bool = false
+var _input_direction : Vector2
 
 func _ready():
-	input_ended.emit()
 	angular_damp = 10
+	input_ended.emit()
 
-func _process(delta):
-	get_input()
-	
 
 func _integrate_forces(state):
-	move()
-	turn()
-
-
-func get_input():
-	if(Input.is_action_pressed("move_left") 
-	or Input.is_action_pressed("move_right") 
-	or Input.is_action_pressed("move_up") 
-	or Input.is_action_pressed("move_down")):
-		start_input()
-	if(!Input.is_action_pressed("move_left")
-	and !Input.is_action_pressed("move_right") 
-	and !Input.is_action_pressed("move_up") 
-	and !Input.is_action_pressed("move_down")):
-		end_input()
+	# has to happen here because we modify constant_torque directly
+	# maybe find a way to make it work with add_constant_torque()?
+	turn(_input_direction)
 
 
 func start_input():
-	if(_is_inputting == false):
-		_is_inputting = true
-		input_started.emit()
+	_is_inputting = true
+	input_started.emit()
 
 
 func end_input():
-	if(_is_inputting == true):
-		_is_inputting = false
-		input_ended.emit()
+	_is_inputting = false
+	input_ended.emit()
 
 
-func move():
+func move(input_direction : Vector2):
+	apply_force(thrust_speed.rotated(rotation))
+	# could call turn(input_direction) here if I figured out add_constant_torque()
+	_input_direction = input_direction
+
+
+func turn(target_direction : Vector2):
+	var rot_change = transform.x.dot(position.direction_to(position + target_direction))
+	constant_torque = rot_change * rotation_speed
+
+
+func _on_input_getter_input_gotten(old_input : Vector2, new_input : Vector2):
+	if(old_input == Vector2.ZERO 
+	and new_input != Vector2.ZERO):
+		start_input()
+	if(old_input != Vector2.ZERO
+	and new_input == Vector2.ZERO):
+		end_input()
 	if(_is_inputting):
-		_input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		apply_force(thrust_speed.rotated(rotation))
-		
-		
-func turn():
-	if(_is_inputting):
-		# dot prod between where we are and where we want to go
-		var rot = transform.x.dot(position.direction_to(position + _input_direction))
-		constant_torque = rot * rotation_speed
+		move(new_input)
